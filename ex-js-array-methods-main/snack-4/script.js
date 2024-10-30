4bd63343cf85544d00864b7bcc62e434f5ae1aae
antoniorusso2 <russoantonio303@outlook.it>
2024-10-30 16:47:48 +0100

    ADD: 4th ex solution

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -3,7 +3,11 @@ const posts = [
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
+const newArr = posts.map((obj) => obj['author']);
+
+console.log(newArr);
+
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
