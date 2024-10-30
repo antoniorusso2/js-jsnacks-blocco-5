00bcb7694e6cf4d1b88adf08721a7202492916d4
antoniorusso2 <russoantonio303@outlook.it>
2024-10-30 16:53:54 +0100

    ADD: 5th ex solution

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,17 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+
+const newArr = nums.filter((el) => el % 2 === 0);
+
+//versione estesa
+
+// const newArr = nums.filter((num) => {
+//   let isEven = num % 2 === 0;
+
+//   if (isEven) {
+//     return true;
+//   }
+// });
+console.log(newArr);
 // Risultato: [2, 8, 4, 12]
